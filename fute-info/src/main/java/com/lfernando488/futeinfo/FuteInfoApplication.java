d9b5dd0a27ff98638e5f456123517b6a793d7e49
Luiz Fernando Geraldo dos Santos <lfernando488@gmail.com>
2021-09-25 18:39:19 -0300

    Criação do projeto

---
+++ fute-info/src/main/java/com/lfernando488/futeinfo/FuteInfoApplication.java
@@ -0,0 +1,13 @@
+package com.lfernando488.futeinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuteInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuteInfoApplication.class, args);
+	}
+
+}
---
+++ fute-info/src/test/java/com/lfernando488/futeinfo/FuteInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfernando488.futeinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuteInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
